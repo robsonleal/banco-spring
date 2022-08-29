aa1f5c98e9b15510805e0137cb0d5d67e6442c31
Robson da Silva Leal <robson_leal@sicredi.com.br>
2022-08-29 17:52:39 -0300

    Arquivos iniciais do projeto springboot

---
+++ src/main/java/com/exercicio/bancospring/BancoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.bancospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/bancospring/BancoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.bancospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
